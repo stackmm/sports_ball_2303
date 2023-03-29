7a701f2628d2f31dd00e1c3ffe96762cdeb6cee8
Steven Stackhouse <stackmm@gmail.com>
2023-03-29 09:58:41 -0600

    add the add_player method

--- lib/team.rb
+++ lib/team.rb
@@ -1,15 +1,25 @@
 class Team
-  attr_reader :name, :city, :roster, :player_count
+  attr_reader :name, :city, :roster, :player_count,
+              :long_term_players, :short_term_players
 
   def initialize(name, city)
     @name = name
     @city = city
     @roster = []
     @player_count = 0
+    @long_term_players = []
+    @short_term_players = []
   end
 
   def add_player(player)
     @roster << player
     @player_count += 1
+
+    if player.contract_length > 24
+      @long_term_players << player
+    else
+      @short_term_players << player
+    end
   end
+  
 end
