fbf8bbcf44f0fa345a671e0751eadc7e410797cc
Steven Stackhouse <stackmm@gmail.com>
2023-03-29 09:39:37 -0600

    add roster and player_count attributes

--- lib/team.rb
+++ lib/team.rb
@@ -1,9 +1,11 @@
 class Team
-  attr_reader :name, :city
+  attr_reader :name, :city, :roster, :player_count
 
   def initialize(name, city)
     @name = name
     @city = city
+    @roster = []
+    @player_count = 0
   end
 
 end
