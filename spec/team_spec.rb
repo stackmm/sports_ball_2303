272b14e45b71bd65060677b88b089820661ab510
Steven Stackhouse <stackmm@gmail.com>
2023-03-29 10:22:56 -0600

    add details tests

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe Team do
     expect(team.short_term_players).to eq([player_2, player_4])
   end
 
-  it 'can find the total value of a team' Dodgers do
+  it 'can find the total value of a team' do
     team = Team.new("Dodgers", "Los Angeles")
     player_1 = Player.new("Michael Palledorous" , 1000000, 36)
     player_2 = Player.new("Kenny DeNunez", 500000, 24)
@@ -63,6 +63,19 @@ RSpec.describe Team do
     expect(team.total_value).to eq(85200000)
   end
 
+  it 'can list the details of a team' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
 
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+  
+    expect(team.details).to eq("{total_value => 85200000, player_count => 4}")
+  end
 
 end
