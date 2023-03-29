4b3ab69385ede813b435224fd7c553c7d74af088
Steven Stackhouse <stackmm@gmail.com>
2023-03-29 09:40:08 -0600

    add roster and player_count tests

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,6 +3,25 @@ require './lib/player'
 require './lib/team'
 
 RSpec.describe Team do
+  it 'exists' do
+    team = Team.new("Dodgers", "Los Angeles")
 
-  
+    expect(team).to be_a(Team)
+  end
+
+  it 'has a roster' do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    expect(team.roster).to eq([])
+  end
+
+  it 'has a player count' do
+    team = Team.new("Dodgers", "Los Angeles")
+    expect(team.player_count).to eq(0)
+  end
+
+  xit 'can add players' do
+
+  end
+
 end
